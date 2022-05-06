52fcf801a5f988bf97e34bd7a48ce9c5019b006d
Francisco Aguiar <maegner.dev@gmail.com>
2022-05-06 15:37:53 +0100

    Update for new farms

--- src/static/js/arbitrum_jones.js
+++ src/static/js/arbitrum_jones.js
@@ -5,17 +5,12 @@ $(function () {
 const JONES_ABI = [{"inputs":[{"internalType":"address","name":"_rewardsDistribution","type":"address"},{"internalType":"address","name":"_rewardsTokenJONES","type":"address"},{"internalType":"address","name":"_stakingToken","type":"address"},{"internalType":"uint256","name":"_rewardsDuration","type":"uint256"},{"internalType":"uint256","name":"_boostedTimePeriod","type":"uint256"},{"internalType":"uint256","name":"_boost","type":"uint256"},{"internalType":"uint256","name":"_id","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_contract","type":"address"}],"name":"AddToContractWhitelist","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_contract","type":"address"}],"name":"RemoveFromContractWhitelist","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"rewardJONES","type":"uint256"}],"name":"RewardAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"rewardJONES","type":"uint256"}],"name":"RewardCompounded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"reward","type":"uint256"}],"name":"RewardPaid","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"rewardJONES","type":"uint256"}],"name":"RewardUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Staked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdrawn","type":"event"},{"inputs":[],"name":"boost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"boostedFinish","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"boostedTimePeriod","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"id","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastUpdateTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"periodFinish","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"rewardPerTokenStoredJONES","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardRateJONES","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardsDistribution","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardsDuration","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"rewardsJONES","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardsTokenJONES","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"stakingToken","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userJONESRewardPerTokenPaid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"whitelistedContracts","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_addr","type":"address"}],"name":"isContract","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastTimeRewardApplicable","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rewardPerToken","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"}],"name":"earned","outputs":[{"internalType":"uint256","name":"JONEStokensEarned","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"stake","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amountJONES","type":"uint256"}],"name":"withdrawRewardTokens","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"compound","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getReward","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"exit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_rewardJONES","type":"uint256"}],"name":"notifyRewardAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_contract","type":"address"}],"name":"addToContractWhitelist","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_contract","type":"address"}],"name":"removeFromContractWhitelist","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
 
 const Pools = [
-  "0xf1a26Cf6309a59794da29B5b2E6fABD3070d470f",
-  "0x360a766F30F0Ba57d2865eFb32502FB800b14dD3",
-  "0x13f6A63867046107780Bc3fEBdeE90E7AfCdfd99",
-  "0xBAc58e8b57935A0B60D5Cb4cd9F6C21049595F04",
-  "0x7eCe38dBE9D61D0d9Bf2D804A87A7d21b5937a56",
-  "0x5723be83199C9Ec68ED0Ac979e98381224870e7f"
+  0,1,2,3,4,5
 ].map(a => ({
-      address: a,
+      address: "0xb94d1959084081c5a11C460012Ab522F5a0FD756",
       abi: JONES_ABI,
-      stakeTokenFunction: "stakingToken",
-      rewardTokenFunction: "rewardsTokenJONES"
+      rewardTokenFunction: "rewardsTokenJONES",
+      poolId:a
   }))
 
 async function main() {
@@ -27,7 +22,7 @@ async function main() {
   var tokens = {};
   const prices = await getArbitrumPrices();
 
-  await loadJonesSynthetixPoolInfo(App, tokens, prices, Pools[1].abi, Pools[1].address, Pools[1].rewardTokenFunction, Pools[1].stakeTokenFunction)
+  //await loadJonesSynthetixPoolInfo(App, tokens, prices, Pools[1].abi, Pools[1].address, Pools[1].rewardTokenFunction, Pools[1].stakeTokenFunction)
 
   let p = await loadMultipleJonesSynthetixPools(App, tokens, prices, Pools)
   _print_bold(`Total staked: $${formatMoney(p.staked_tvl)}`);
@@ -41,7 +36,7 @@ async function main() {
 async function loadMultipleJonesSynthetixPools(App, tokens, prices, pools) {
   let totalStaked  = 0, totalUserStaked = 0, individualAPRs = [];
   const infos = await Promise.all(pools.map(p =>
-    loadJonesSynthetixPoolInfo(App, tokens, prices, p.abi, p.address, p.rewardTokenFunction, p.stakeTokenFunction)));
+    loadJonesSynthetixPoolInfo(App, tokens, prices, p.abi, p.address, p.rewardTokenFunction, p.poolId)));
   for (const i of infos) {
     let p = await printArbitrumSynthetixPool(App, i, "arbitrum");
     totalStaked += p.staked_tvl || 0;
@@ -55,14 +50,12 @@ async function loadMultipleJonesSynthetixPools(App, tokens, prices, pools) {
 }
 
 async function loadJonesSynthetixPoolInfo(App, tokens, prices, stakingAbi, stakingAddress,
-  rewardTokenFunction, stakeTokenFunction) {
+  rewardTokenFunction, poolId) {
     const STAKING_POOL = new ethers.Contract(stakingAddress, stakingAbi, App.provider);
     const STAKING_MULTI = new ethcall.Contract(stakingAddress, stakingAbi);
 
-    if (!STAKING_POOL.callStatic[stakeTokenFunction]) {
-      console.log("Couldn't find stake function ", stakeTokenFunction);
-    }
-    const stakeTokenAddress = await STAKING_POOL.callStatic[stakeTokenFunction]();
+  
+    const stakeTokenAddress = await STAKING_POOL.poolInfo(poolId).lpToken;
 
     const rewardTokenAddress = await STAKING_POOL.callStatic[rewardTokenFunction]();
 
@@ -105,12 +98,10 @@ async function loadJonesSynthetixPoolInfo(App, tokens, prices, stakingAbi, staki
         prices[stakeTokenAddress]?.usd ?? getParameterCaseInsensitive(prices, stakeTokenAddress)?.usd;
     const rewardTokenPrice = getParameterCaseInsensitive(prices, rewardTokenAddress)?.usd;
 
-    const calls = [STAKING_MULTI.periodFinish(), STAKING_MULTI.rewardRateJONES(),
-      STAKING_MULTI.balanceOf(App.YOUR_ADDRESS), STAKING_MULTI.earned(App.YOUR_ADDRESS),
-      STAKING_MULTI.boost(), STAKING_MULTI.boostedFinish()]
-    const [periodFinish, rewardRate, balance, earned_, boost, boostedFinish] = await App.ethcallProvider.all(calls);
+    const calls = [STAKING_MULTI.periodFinish(), STAKING_MULTI.jonesPerSecond(), STAKING_MULTI.deposited(poolId, App.YOUR_ADDRESS), STAKING_MULTI.pendingJones(poolId, App.YOUR_ADDRESS)]
+    const [rewardRate, balance, earned_,] = await App.ethcallProvider.all(calls);
 
-    const weeklyRewards = (Date.now() / 1000 > periodFinish) ? 0 : ((Date.now() / 1000 > boostedFinish) ? rewardRate : rewardRate * boost / 100) / 1e18 * 604800;
+    const weeklyRewards =  (rewardRate / 1e18) * 604800;
 
     const usdPerWeek = weeklyRewards * rewardTokenPrice;
 
